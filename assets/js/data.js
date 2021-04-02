333c5d447d497d543c388902db00e43a6c6f37a0
tintinmar1995 <martin.masson@protonmail.com>
2021-04-02 19:12:30 +0200

    Retrieve availability from api and update layout

--- assets/js/data.js
+++ assets/js/data.js
@@ -10,3 +10,44 @@ function cmdStatisticLoaded(){
     document.getElementById('stat-'+key).innerText = cmd_data['overview'][key]
   }
 }
+
+// AVAILABILITY
+
+var availability = {'aspb' : true, 'aspv' : true, 'fraise' : true}
+const colorBkg = {'aspb' : "white", 'aspv' : "green", 'fraise' : "red"}
+const colorI = {'aspb' : "green", 'aspv' : "white", 'fraise' : "white"}
+
+function updateAvailability(){
+  for (item of ['aspb', 'aspv', 'fraise']) {
+    if(availability[item]){
+      document.getElementById(item+'-container').classList.remove('w3-teal')
+      document.getElementById(item+'-container').classList.add('w3-'+colorBkg[item])
+      document.getElementById(item+'-container').childNodes[3].childNodes[1].childNodes[0].style.color=colorI[item]
+      document.getElementById(item+'-container').childNodes[3].childNodes[1].childNodes[0].classList.remove('fa-spinner')
+      document.getElementById(item+'-container').childNodes[3].childNodes[1].childNodes[0].classList.remove('fa-times-circle')
+      document.getElementById(item+'-container').childNodes[3].childNodes[1].childNodes[0].classList.add('fa-check-circle')
+    } else {
+      document.getElementById(item+'-container').classList.remove('w3-'+colorBkg[item])
+      document.getElementById(item+'-container').classList.add('w3-teal')
+      document.getElementById(item+'-container').childNodes[3].childNodes[1].childNodes[0].style.color="red"
+      document.getElementById(item+'-container').childNodes[3].childNodes[1].childNodes[0].classList.remove('fa-spinner')
+      document.getElementById(item+'-container').childNodes[3].childNodes[1].childNodes[0].classList.remove('fa-check-circle')
+      document.getElementById(item+'-container').childNodes[3].childNodes[1].childNodes[0].classList.add('fa-times-circle')
+    }
+  }
+}
+
+$.get("https://api.champ-ramard.fr/settings.php", function(result){
+  for(var k in result) {
+    if(result[k]['STR_KEY']=='aspb' | result[k]['STR_KEY']=='aspv' | result[k]['STR_KEY']=='fraise'){
+      if(result[k]['STR_VALUE']=='true'){
+        availability[result[k]['STR_KEY']] = true
+      }else if(result[k]['STR_VALUE']=='false'){
+        availability[result[k]['STR_KEY']] = false
+      }else{
+        availability[result[k]['STR_KEY']] = result[k]['STR_VALUE']
+      }
+    }
+  }
+  updateAvailability()
+})
